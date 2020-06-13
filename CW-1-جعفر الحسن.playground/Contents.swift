4b2aee976ce20f7aaa97b3dab07ef408750985d3
Jafar-yousef-AlHasan-112 <j99j55@gmail.com>
2020-06-13 20:34:48 +0300

    done

---
+++ CW-1-جعفر الحسن.playground/Contents.swift
@@ -0,0 +1,41 @@
+
+
+let name = "jafar alhasan"
+
+var myage = 17
+var hobby = "watching Movies"
+var hobby2 = " history"
+var hieght = 167.64
+ 
+print("my name is", name)
+print("my age is", myage)
+print("my hobbies are", hobby,"and",hobby2)
+print("and my hieght is",hieght)
+
+
+let Friendname = "yousef"
+var Friendage = 17
+var Friendhobby = "watching Movies"
+var Friendhobby2 = "music"
+var friendhieght = 180.1
+
+//comparing
+var subage_difference = Friendage - myage
+
+name == Friendname
+name != Friendname
+
+hobby == Friendhobby
+hobby != Friendhobby
+hobby == Friendhobby2
+hobby != Friendhobby2
+hobby2 == Friendhobby2
+hobby == Friendhobby2
+hobby != Friendhobby2
+
+hieght == friendhieght
+hieght <= friendhieght
+hieght >= friendhieght
+hieght > friendhieght
+hieght < friendhieght
+hieght != friendhieght
